c47991f601a68f8448c29f9ac37de495df5f53c9
julieweeds <julie.weeds@gmail.com>
2012-12-20 15:31:34 +0000

    Added some documentation

--- src/gigacount.py
+++ src/gigacount.py
@@ -1,5 +1,16 @@
 import re
 import sys
+###
+#gigacount takes an extracted feature file (from a corpus such as the gigaword) and counts the number of instance
+# occurrences of each noun,verb,adj and adverb - it only looks at the first word in each line as this is the instance
+# (other words in the line are the feature)
+#
+#it then takes the WN multiple choice test and looks up each unigram in its word lists - it checks for 1, 2, 3, 4, 5
+#or more occurrences.  It also computes 2 unions 1) for nouns and adjs and 2) for all pos.  This is for a threshold
+#frequency value
+
+
+
 #from thesaurus import Thesaurus
 from WNEval import Eval
 
@@ -20,31 +31,39 @@ inputname=dirname+filename
 filename=datadir+myfile
 
 class PoSset:
+    #list of words and frequencies for particular POS
     entrycount=0
 
     def __init__(self,pos):
         PoSset.entrycount += 1
         self.pos = pos
         self.wordcountlist = {}
-        self.testcounts={1:0,2:0,3:0,4:0,5:0}
+        self.testcounts={1:0,2:0,3:0,4:0,5:0} #each key is mapped to how many words (that are looked up) have occurred that number of times or more
+
 
     def addword(self,word):
+        #increase seen frequency of word by 1
         if word in self.wordcountlist:
             self.wordcountlist[word] +=1
         else:
             self.wordcountlist[word] = 1
 
     def getword(self,word):
+        #return seen frequency of word
         if word in self.wordcountlist:
             return self.wordcountlist[word]
         else:
             return 0
 
     def displayall(self):
+        #print out all words and frequencies
         print self.pos
         for word in self.wordcountlist:
             print word, ":", self.getword(word)
+
     def check(self,word):
+        #perform test on word.  If in list, update total number of words that have occurred over threshold levels
+        #in test counts.  Also return frequency so that union totals can be updated appropriately.
         freq = self.getword(word)
         for key in self.testcounts.keys():
             if (freq >= key):
@@ -55,7 +74,7 @@ class PoSset:
 
 class Gigadict:
     entrycount=0
-    linePatt = re.compile("(.*)/(.)")
+    linePatt = re.compile("(.*)/(.)") #match the first word in each line and split into word and POS tag
     threshold = 1
 
 
@@ -67,9 +86,9 @@ class Gigadict:
         self.advs=PoSset("adv")
         self.linecount=0
         self.readfile(filename)
-        self.tests=0
-        self.any=0
-        self.nj=0
+        self.tests=0  #number of words looked up
+        self.any=0    #union of words occurring in any list
+        self.nj=0  #union of words occurring in noun and adj list
 
 
     def process(self,line):
@@ -93,7 +112,7 @@ class Gigadict:
         for line in f:
             self.linecount +=1
             self.process(line.rstrip())
-            #print self.linecount
+            if((self.linecount % 1000)==0):print "Read line: "self.linecount
         f.close()
 
     def breakpoint(self):
@@ -140,6 +159,7 @@ def posset_test():
 
 #posset_test()
 
+print "Coverage evaluation started for ",filename," and ",inputname
 mydata = Eval(filename,'Testing')
 mydata.readfile()
 print "Read" + filename
